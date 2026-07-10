d9c634e8b16de209afc6d93c0c28a4e28d140754
bag8516-dev <bag8516@gmail.com>
2026-07-10 09:34:36 +0900

    Remove non-site files accidentally added

--- start-server.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-set PATH=C:\Program Files\nodejs;%PATH%
-set PATH=C:\Users\박세진\AppData\Roaming\npm;%PATH%
-"C:\Users\박세진\AppData\Roaming\npm\serve.cmd" -s "C:\Users\박세진\OneDrive\Desktop\업무관련\MPC-Escrow\frontend" -l 3000
